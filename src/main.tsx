1326adac3f5099a364351bbdb2d82ab8cfcc7e7e
Piyush Kanojiya <raat131221@gmail.com>
2026-09-05 22:15:41 +0530

    feat: mount Clerk provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ClerkProvider } from '@clerk/react';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from './context/ThemeContext';
 import { SmoothScroll } from './components/SmoothScroll';
 
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!clerkPublishableKey) {
+  console.warn('VITE_CLERK_PUBLISHABLE_KEY is not configured. Clerk authentication will not initialize.');
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <SmoothScroll>
-        <App />
-      </SmoothScroll>
-    </ThemeProvider>
+    <ClerkProvider publishableKey={clerkPublishableKey}>
+      <ThemeProvider>
+        <SmoothScroll>
+          <App />
+        </SmoothScroll>
+      </ThemeProvider>
+    </ClerkProvider>
   </StrictMode>
 );
