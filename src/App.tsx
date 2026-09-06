a764b1fad0d5eb1314badc39044e5cb717f41251
Piyush Kanojiya <raat131221@gmail.com>
2026-09-06 16:37:58 +0530

    style: load profile setup theme overrides

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { MotionSystem } from './components/MotionSystem';
 import './styles/profile-setup.css';
 import './styles/login.css';
 import './styles/login-modern.css';
+import './styles/profile-setup-theme.css';
 
 const LandingPage = lazy(() => import('./pages/LandingPage').then((module) => ({ default: module.LandingPage })));
 const LoginPage = lazy(() => import('./pages/LoginPage').then((module) => ({ default: module.LoginPage })));
