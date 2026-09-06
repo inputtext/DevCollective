0a61d0e1a648fa6bf821a1faf303e4bf5f9b9313
Piyush Kanojiya <raat131221@gmail.com>
2026-09-06 18:30:49 +0530

    style: load refined workspace control styles

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import './styles/login-modern.css';
 import './styles/profile-setup-theme.css';
 import './styles/workspace-pastel.css';
 import './styles/dark-theme.css';
+import './styles/workspace-controls.css';
 
 const LandingPage = lazy(() => import('./pages/LandingPage').then((module) => ({ default: module.LandingPage })));
 const LoginPage = lazy(() => import('./pages/LoginPage').then((module) => ({ default: module.LoginPage })));
