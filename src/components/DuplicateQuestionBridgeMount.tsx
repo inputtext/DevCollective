7bf662e5da2c60b7026460084ea4f4afd9f55cdb
Piyush Kanojiya <raat131221@gmail.com>
2026-09-12 15:23:33 +0530

    chore: expose duplicate question bridge mount

---
+++ src/components/DuplicateQuestionBridgeMount.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { DuplicateQuestionBridge } from './DuplicateQuestionBridge';
+
+export const DuplicateQuestionBridgeMount: React.FC = () => <DuplicateQuestionBridge />;
